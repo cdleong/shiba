3aade7d39a1fa4aedeed86d94b282e07f657e14d
Colin Leong <--unset>
2021-08-10 16:54:42 -0400

    CDL: more edits to pretrain_ner.py, still unfinished

--- training/finetune_ner.py
+++ training/finetune_ner.py
@@ -17,11 +17,11 @@ from pathlib import Path
 from clearml import Task, Dataset
 
 #TODO: adapt this to NER! 
-# * [] vocab size
+# * [x] vocab size/class count
 # * [] process_examples needs fixing
 # * [] clearml integration: init task
-# * [] clearml integration: download dataset
-# * [] clearml integration: download pretrained model
+# * [] clearml integration: download dataset from masakhaNER fork
+# * [] clearml integration: download pretrained model from clearML
 
 
 def load_and_fix_state_dict(path_to_pytorch_model:Path):
@@ -45,6 +45,8 @@ def load_and_fix_state_dict(path_to_pytorch_model:Path):
 
     return state_dict_with_fixed_keys
 
+
+
 def main():
     transformers.logging.set_verbosity_info()
     parser = HfArgumentParser((ShibaNERArgs,))
@@ -56,26 +58,71 @@ def main():
 
         tokenizer = CodepointTokenizer()
 
-        def process_example(example: Dict) -> Dict:
-            tokens = example['tokens']  # cdleong: should be a list of characters
-            text = ''.join(tokens)  # cdleong: combines the list of characters to one big string. 
-            labels = [0]  # CLS token
-            for token in tokens:
-                new_label_count = len(token)
-                new_labels = [1] + [0] * (new_label_count - 1)
-                labels.extend(new_labels)
+        # def process_example(example: Dict) -> Dict:
+        #     tokens = example['tokens']  # cdleong: should be a list of characters
+        #     text = ''.join(tokens)  # cdleong: combines the list of characters to one big string. 
+        #     labels = [0]  # CLS token
+        #     for token in tokens:
+        #         new_label_count = len(token)
+        #         new_labels = [1] + [0] * (new_label_count - 1)
+        #         labels.extend(new_labels)
 
-            input_ids = tokenizer.encode(text)['input_ids']
+        #     input_ids = tokenizer.encode(text)['input_ids']
+
+        #     return {
+        #         'input_ids': input_ids,
+        #         'labels': labels
+        #     }
+
+        def process_example(example: Dict) -> Dict:
+            '''Designed to process MasakhaNER examples from our fork at https://github.com/cdleong/masakhane-ner
+            Based on process_example commented out above, but specifically pulling the "ner_tags" field
+            See also https://github.com/cdleong/masakhane-ner/blob/main/custom_huggingface_loading_script.py
+            '''
+            original_data = example['tokens']
+            text = ''.join(original_data)  # join all the character together 
+
+            # 
+            tokenized_text = tokenizer.encode(text) # returns a dict with keys "input_ids" and "attention_mask"... I think
+            input_ids = tokenized_text['input_ids']  # pull out the input_ids, which should be ints
+
+            labels = [0] # make a list with "O" (outside) at the start, because tokenizer adds a CLS token at the beginning
+
+            masakhaNERlabels=example["ner_tags"]  # masakhaNER loading script gives us ints, we just rename them
+            labels.extend(masakhaNERlabels) # now the input_ids and labels are the same length!
+
 
             return {
-                'input_ids': input_ids,
-                'labels': labels
+            'input_ids': input_ids,
+            'labels': labels
             }
 
         model_hyperparams = get_model_hyperparams(training_args)
 
-        # beginning of word or not beginning of word
-        model = ShibaForSequenceLabeling(2, **model_hyperparams)
+        
+        # label_count=2 # beginning of word or not beginning of word are the two labels for word segmentation
+        
+        # MasakhaNER has 9 possible labels. 
+        # There are 4 entity types: [ORG, PER, DATE, LOC] entities
+        # Each token is annotated as either B-{entity label} to mark the beginning of an entity, 
+        # or I-{entity label} to mark the continuation, 
+        # or O for outside/not part of an entity. So possible labels include B-ORG, I-ORG, B-PER, O...
+        # Example from MasakhaNER swa dev set: 
+        # Masoko O
+        # ya O
+        # Japan B-LOC
+        # , O
+        # China B-LOC
+        # na O
+        # Thailand B-LOC
+        # yalikuwa O 
+        # 
+        # Another example: 
+        # Mike B-PER
+        # Pompeo I-PER       
+        masakhane_ner_label_count=9 
+        label_count=masakhane_ner_label_count
+        model = ShibaForSequenceLabeling(label_count, **model_hyperparams)
 
         if training_args.resume_from_checkpoint:
             print('Loading and using base shiba states from', training_args.resume_from_checkpoint)
@@ -102,10 +149,11 @@ def main():
 
             return encoded_example
 
-    dep = load_dataset('universal_dependencies', 'ja_gsd')
-    dep = dep.map(process_example, remove_columns=list(dep['train'][0].keys()))
+    # TODO: edit this to load our forked masakhaNER data
+    ner_dataset = load_dataset('universal_dependencies', 'ja_gsd')
+    ner_dataset = ner_dataset.map(process_example, remove_columns=list(ner_dataset['train'][0].keys()))
 
-    os.environ['WANDB_PROJECT'] = 'shiba'
+    # os.environ['WANDB_PROJECT'] = 'shiba'
 
     def compute_metrics(pred: EvalPrediction) -> Dict:
 
@@ -142,13 +190,13 @@ def main():
     trainer = Trainer(model=model,
                       args=training_args,
                       data_collator=data_collator,
-                      train_dataset=dep['train'],
-                      eval_dataset=dep['validation'],
+                      train_dataset=ner_dataset['train'],
+                      eval_dataset=ner_dataset['validation'],
                       compute_metrics=compute_metrics
                       )
 
     trainer.train()
-    posttrain_metrics = trainer.predict(dep['test']).metrics
+    posttrain_metrics = trainer.predict(ner_dataset['test']).metrics
     print(posttrain_metrics)
 
 
